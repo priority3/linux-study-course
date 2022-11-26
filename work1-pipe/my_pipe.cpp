645f2d0b3d285b32cb47effeb4ad66c4ea65cf1f
priority3 <prioritycc@outlook.com>
2022-11-26 05:09:51 -0500

    fix: some code

--- work1-pipe/my_pipe.cpp
+++ work1-pipe/my_pipe.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <iostream>
 #include <stdlib.h>
 #include <errno.h>
 //#include <cstdlib>
@@ -12,48 +12,48 @@ int main(){
 	int pid1,pid2;
 
 	// char 
-	char OutPipe[100],InPipe[100];
+	char InPipe[100];
+
+	pipe(fd);
 
 	// create pipe
-	while((pid2 = fork()) == -1);
-
+	//while((pid2 = fork()) == -1);
+	pid2 = fork();
 	// child process 2
 	if(pid2 == 0){
 		// do something
+		std::cout<<"child 2 \n";
 		lockf(fd[1],1,0);
-		sprintf(OutPipe,"Child process 2 is sending a message!");
-		write(fd[1],OutPipe,50);
-		sleep(1);
+		write(fd[1],"Child process 2 is sending a message! \n",50);
 		lockf(fd[1],0,0);
-		exit(2);
+		exit(0);
 	}
-
 	else {
 		// relative child 2 process of parent
-		while ((pid1 = fork()) == -1);
+		//while ((pid1 = fork()) == -1);
+		pid1 = fork();
 		if(pid1 == 0){
 			// child process 1
-			// do something
 			lockf(fd[1],1,0);
-			sprintf(OutPipe,"Child process 1 is sending a message!");
-			write(fd[1],OutPipe,50);
-			sleep(1);
+		  std::cout<<"child 1 \n";
+			write(fd[1],"child process 1 is sending a message",50);
 			lockf(fd[1],0,0);
-			exit(1);
+			exit(0);
 		}else {
 			// relative child 1 process of parent
-			read(fd[0],InPipe,50);
-			printf(" %s \n",InPipe);
-			wait();
-			read(fd[0],InPipe,50);
-			printf(" %s \n",InPipe);
-
+			while(-1 == read(fd[0],InPipe,sizeof(InPipe))){
+				sleep(1);
+				std::cout<<"wait... \n";
+			}
+		  std::cout<<InPipe<<"\n";			
+			while(-1 == read(fd[0],InPipe,sizeof(InPipe))){
+				sleep(1);
+				std::cout<<"wait... \n";
+			}
+			std::cout<<InPipe<<"\n";
 			exit(0);
 		}
-
 	}
-
-
 
 	return 0;
 }
